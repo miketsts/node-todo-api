f10b22c79c0f5c4206dbad7e0f59011dfc0419a6
Michael <Michael>
2017-10-25 22:09:54 +0300

    Extract config.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,4 @@
-var env = process.env.NODE_ENV || 'development';
-console.log('NODE_ENV ****', env);
-
-if (env === 'development') {
-  process.env.port = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.port = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
+require('./config/config');
 
 const _ = require('lodash');
 const express = require('express');
